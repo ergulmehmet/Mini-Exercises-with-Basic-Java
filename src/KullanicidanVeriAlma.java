07019b66c3db58b790701e35dd0644d88f642bf4
Mehmet <m.ergul06@gmail.com>
2024-02-26 16:47:02 +0300

    KullanicidanVeriAlma kücük proje

---
+++ src/KullanicidanVeriAlma.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class KullanicidanVeriAlma {
+    public static void main(String[] args) {
+        int a;
+        Scanner input = new Scanner(System.in);
+        System.out.println("a sayısını giriniz : ");
+        a = input.nextInt();
+        System.out.println(a);
+
+
+
+
+    }
+}
